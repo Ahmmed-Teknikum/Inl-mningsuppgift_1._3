d22e4f7d4ed1b28e6ba0b0c672a9644173159994
ahka07001 <ahka07001@E-PW05CGQT>
2024-09-12 14:30:20 +0200

    Add project files.

---
+++ Inlämningsuppgift_1._3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Inlämningsuppgift_1._3
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("    /\\");
+            Console.WriteLine("   /  \\");
+            Console.WriteLine("  / \" \"\\");
+            Console.WriteLine(" /      \\");
+            Console.WriteLine("/________\\");
+        }
+    }
+}
